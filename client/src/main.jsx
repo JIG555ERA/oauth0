73bd403ca77b4e7b4f88a50b9922b83ce5532ab3
JIG ERA <jig111era@gmail.com>
2025-09-14 21:45:06 +0530

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,17 +6,15 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Auth0Provider
-                domain="jig-era.us.auth0.com"
-                clientId="UtNP3XRtsZ0lRC60JLpQP6qSc6m69bLM"
-                authorizationParams={{
-                redirect_uri: window.location.origin
+    <Auth0Provider
+    clientId="UtNP3XRtsZ0lRC60JLpQP6qSc6m69bLM"
+    domain="jig-era.us.auth0.com"
+    authorizationParams={{
+    redirect_uri: window.location.origin
                 }}
-            >
-                <App />
-            </Auth0Provider>,
+    >
+        <BrowserRouter>
+            <App />
         </BrowserRouter>
-    </StrictMode>,
+    </Auth0Provider>,
 )

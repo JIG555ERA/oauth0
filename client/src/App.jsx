7a9f312fa2c3249685a348e3428444c21fb6383f
JIG ERA <jig111era@gmail.com>
2025-09-14 21:37:42 +0530

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route} from 'react-router-dom'
 import HomePage from './components/pages/HomePage'
+import AboutPage from './components/pages/AboutPage'
 
 const App = () => {
     return (
         <div className='font-[Orbitron] '>
             <Routes>
                 <Route path='/' element={<HomePage />} />
+                <Route path='/about' element={<AboutPage />} />
             </Routes>
         
         </div>
---
+++ client/src/components/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../layout/Header'
+
+const AboutPage = () => {
+    return (
+        <div
+        className='w-full h-screen bg-[#0E0E0E] text-[#F9F9F9] flex justify-center items-center'>
+            <Header />
+        </div>
+    )
+}
+
+export default AboutPage
